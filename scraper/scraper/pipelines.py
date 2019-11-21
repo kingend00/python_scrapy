1f63f6ced2748bd4bd6464c95917436d0042e744
Hydraw <hoangnx1298@gmail.com>
2019-11-21 23:27:38 +0700

    done insert into db mysql

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -6,39 +6,43 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #import mysql.connector
 
+import mysql.connector
+
 class ScraperPipeline(object):
 
-    def process_item(self,item,spider):
-        print('Pipeline: '+ item['title'][0])
-        return item
-    # def __init__(self):
-    #     self.create_connection()
-    #     self.create_table()
-    
-    # def create_connection(self):
-    #     self.conn = mysql.connector.connect(
-    #         host = 'localhost',
-    #         user = 'hoangx',
-    #         passwd = 'Hoang_01',
-    #         database = 'python_scrapy'
-    #     )
-    #     self.curr = self.conn.cursor()
-    
-    # def create_table(self):
-    #     self.curr.execute(""" DROP TABLE IF EXISTS jokes """)
-    #     self.curr.execute(""" create table jokes(
-    #                     question text,
-    #                     answer text
-    #                     ) """)
-
     # def process_item(self,item,spider):
-    #     self.store_db(item)
+    #     print('Pipeline: '+ item['title'][0])
     #     return item
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+    
+    def create_connection(self):
+        self.conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            passwd = 'root',
+            database = 'python_scrapy'
+        )
+        self.curr = self.conn.cursor()
+    
+    def create_table(self):
+        self.curr.execute(""" DROP TABLE IF EXISTS myquotes """)
+        self.curr.execute(""" create table myquotes(
+                        title text,
+                        author text,
+                        tag text
+                        ) """)
 
-    # def store_db(self,item):
-    #     self.curr.execute(""" insert into jokes values (%s,%s) """,(
-    #         item['question'][0],
-    #         item['answer'][1],
-    #     ))
-    #     self.conn.commit() 
+    def process_item(self,item,spider):
+        self.store_db(item)
+        return item
+
+    def store_db(self,item):
+        self.curr.execute(""" insert into myquotes values (%s,%s,%s) """,(
+            item['title'][0],
+            item['author'][0],
+            item['tag'][0],
+        ))
+        self.conn.commit() 
 
